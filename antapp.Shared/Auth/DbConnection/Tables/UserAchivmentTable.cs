f604700fcba0016604bf7937864be5fbaabff832
Olaf <olaf.szajda@gmail.com>
2024-04-24 03:10:05 +0200

    user-menu

---
+++ antapp.Shared/Auth/DbConnection/Tables/UserAchivmentTable.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace antapp.Shared.Auth.DbConnection.Tables;
+
+[Table("achivment_user")]
+public class UserAchivmentTable
+{
+    [Key]
+    public required int id { get; set; }
+    [ForeignKey("userid")]
+    public required string userid { get; set; }
+    [ForeignKey("achivmentid")]
+    public virtual AchivmentTable Achivment { get; set; }
+    public required DateOnly aquiredate { get; set; }
+    public required string progress {  get; set; }
+}
--- antapp.UserMenu/Services/UserAntsService.cs
+++ antapp.UserMenu/Services/UserAntsService.cs
@@ -1,5 +1,7 @@
-﻿using antapp.Shared.Auth.DbConnection;
+﻿using antapp.Shared.Auth;
+using antapp.Shared.Auth.DbConnection;
 using antapp.UserMenu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
 
@@ -9,19 +11,30 @@ namespace antapp.UserMenu.Services;
 public interface IUserAntsService
 {
     Task<List<UserAnt>> GetUserAnts(string userId);
+    Task<List<UserAchivment>> GetUserAchivments(string userId);
 }
 
 internal class UserAntService : IUserAntsService
 {
     private readonly antappDbContext _db;
+    private readonly UserManager<User> _userManager;
 
-    public UserAntService(antappDbContext db)
+    public UserAntService(antappDbContext db, UserManager<User> userManager)
     {
         _db = db;
+        _userManager = userManager;
+    }
+
+    public async Task<string> GetUserName(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        var userName = user.UserName;
+        return userName;
     }
 
     public async Task<List<UserAnt>> GetUserAnts(string userId)
     {
+        var userName = GetUserName("9eb1f47a-d5f1-43d9-b5bc-74e91b5fda99");
         return await _db.UserAnts
             .Where(x => x.userid == userId)
             .Select(x => new UserAnt
@@ -32,6 +45,23 @@ internal class UserAntService : IUserAntsService
                 ImageUrl = x.Ants.imageurl,
                 CatchDate = x.catchdate,
                 CatchLocation = x.Location.locationname
+            })
+            .ToListAsync();
+    }
+
+    public async Task<List<UserAchivment>> GetUserAchivments(string userId)
+    {
+        return await _db.UserAchivments
+            .Where(x => x.userid == userId)
+            .Select(a => new UserAchivment
+            {
+                Id = a.Achivment.id,
+                Name = a.Achivment.achivmentname,
+                Description = a.Achivment.description,
+                AquireDate = a.aquiredate,
+                ImageUrl = a.Achivment.imageurl,
+                Requirement = a.Achivment.requirement,
+                Proggress = a.progress
             })
             .ToListAsync();
     }
